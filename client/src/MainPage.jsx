100277973c646ff49b22a983b2b785dacfbfa7bc
murshidahmed <murshidahmedneon3@gmail.com>
2025-06-28 07:38:40 +0530

    some tailwind css changes

--- client/src/MainPage.jsx
+++ client/src/MainPage.jsx
@@ -4,9 +4,9 @@ import "tailwindcss";
 export default function MainPage() {
   return (
     <div>
-      <h1 className='text-5xl font-bold my-5'>Convert Your Currencies Today</h1>
-      <p className='opacity-40'>Welcome to "Convert Your Currencies Today"! This application allows you
-        to easily convert currencies based on the latest exchange rates. Whether
+      <h1 className='text-5xl font-bold my-5 text-center text-blue-300'>Convert Your Currencies Today</h1>
+      <p className='text-lg opacity-40 text-center mx-30 text-blue-300'>Welcome to "Convert Your Currencies Today"! This application allows you
+        to easily convert currencies based on the latest exchangec rates. Whether
         you're planning a trip, managing your finances, or simply curious about
         the value of your money in different currencies, this tool is here to
         help.</p>
