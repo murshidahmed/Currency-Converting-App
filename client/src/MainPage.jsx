7cf98765d22fa9ecc579f28283b801a3845ffaed
murshidahmed <murshidahmedneon3@gmail.com>
2025-06-30 00:29:28 +0530

    some changes

--- client/src/MainPage.jsx
+++ client/src/MainPage.jsx
@@ -21,14 +21,14 @@ export default function MainPage() {
 
             <div className="mb-5">
               <label htmlFor='' className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Source Currency</label>
-              <select name="" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+              <select name="" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                 <option value="">Select the currency</option>
               </select>
             </div>
 
             <div className="mb-5">
               <label htmlFor='' className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Target Currency</label>
-              <select name="" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+              <select name="" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                 <option value="">Select the currency</option>
               </select>
             </div>
